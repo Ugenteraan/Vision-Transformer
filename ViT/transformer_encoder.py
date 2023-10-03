269e0e3d5dd5caf65fa09fe98d6da69ab11a5746
Ugenteraan Manogaran <m.ugenteraan15@gmail.com>
2023-10-04 00:42:26 +0800

    initialize transformer encoder block module

---
+++ ViT/transformer_encoder.py
@@ -0,0 +1,14 @@
+'''Transformer Encoder block module.
+'''
+
+
+import torch
+import torch.nn as nn
+
+
+
+class TransformerEncoderBlock(nn.Module):
+    '''A single transformer encoder block.
+    '''
+
+    def __init__(self, )
